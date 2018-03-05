25480e6df46d7a0d12d7c6be165ea8cc13b26645
João Carlos Brasileiro <jcbrasileiro@hotmail.com>
2018-03-05 01:38:02 -0300

    [add]

---
+++ maven-project-maker-core/src/main/java/org/npe4j/mpmaker/core/builder/XMLProjectObjectModelBuilder.java
@@ -0,0 +1,9 @@
+package org.npe4j.mpmaker.core.builder;
+
+import org.npe4j.mpmaker.commons.dp.DPBuilder;
+import org.npe4j.mpmakerxml.xml.pom.XMLProjectObjectModel;
+
+public interface XMLProjectObjectModelBuilder
+    extends
+    DPBuilder<XMLProjectObjectModel> {
+}
